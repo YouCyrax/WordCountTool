ea7daeca1395206ddcb4be5eeb88774843b1516e
Younes <64960393+YouCyrax@users.noreply.github.com>
2022-05-06 20:32:02 +0100

    Update WordCount.java

--- WordCountTool/src/main/java/com/countingWords/WordCount.java
+++ WordCountTool/src/main/java/com/countingWords/WordCount.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.countingWords;
 
-/**
- *
- * @author timgadinformatique
+/*
+ * @author: Younes Seghirou
  */
 public class WordCount extends javax.swing.JFrame {
 
